64c4c8a3c95b2bbaa9f16224719b6e0bdaee7d25
Forhad Zinnah <forhad@Forhads-Mac-mini.local>
2023-02-21 16:47:38 -0800

    upto redux on authentication tab

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,26 @@
 const router = require('express').Router()
 
+router.post('/login', async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.send({ token: await user.generateToken() })
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists')
+    } else {
+      next(err)
+    }
+  }
+})
+
 router.use((req, res, next) => {
   const err = new Error('API route not found')
   err.status = 400
---
+++ server/db/Models/User.js
@@ -0,0 +1,59 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const SALT_ROUNDS = 5
+
+const User = db.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+  },
+  githubId: {
+    type: Sequelize.INTEGER,
+  },
+})
+
+module.exports = User
+
+User.prototype.correctPassword = function (candidatePwd) {
+  return bcrypt.compare(candidatePwd, this.password)
+}
+
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT)
+}
+
+User.authenticate = async function ({ username, password }) {
+  const user = await this.findOne({ where: { username } })
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error('Incorrect username/password')
+    error.status = 401
+    throw error
+  }
+  return user.generateToken()
+}
+User.findByToken = async function (token) {
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT)
+    const user = User.findByPk(id)
+    if (!user) {
+      throw 'nooo'
+    }
+    return user
+  } catch (ex) {
+    const error = Error('bad token')
+    error.status = 401
+    throw error
+  }
+}
+
+const hashPassword = async (user) => {
+  //in case the password has been changed, we want to encrypt it with bcrypt
+  if (user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS)
+  }
+}
